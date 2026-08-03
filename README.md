73d6750601e52378fdd8cbf49dd70d5308b448a9
Giovanni Belizario <giovanni.s.belizario@gmail.com>
2026-08-02 21:19:47 -0300

    Projeto inicial

---
+++ README.md
@@ -0,0 +1,12 @@
+# Cadastro de Livros
+
+Projeto desenvolvido em Python para praticar listas, funções, estruturas de repetição e busca de dados.
+
+## Tecnologias
+
+- Python 3
+
+## Funcionalidades
+
+- Cadastro de livros
+- Busca por título
---
+++ cadastro_livros.py
@@ -0,0 +1,49 @@
+def titulo():
+    print('''
+██████╗░██████╗░░█████╗░░█████╗░██╗░░░██╗██████╗░░█████╗░███╗░░██╗██████╗░░█████╗░
+██╔══██╗██╔══██╗██╔══██╗██╔══██╗██║░░░██║██╔══██╗██╔══██╗████╗░██║██╔══██╗██╔══██╗
+██████╔╝██████╔╝██║░░██║██║░░╚═╝██║░░░██║██████╔╝███████║██╔██╗██║██║░░██║██║░░██║
+██╔═══╝░██╔══██╗██║░░██║██║░░██╗██║░░░██║██╔══██╗██╔══██║██║╚████║██║░░██║██║░░██║
+██║░░░░░██║░░██║╚█████╔╝╚█████╔╝╚██████╔╝██║░░██║██║░░██║██║░╚███║██████╔╝╚█████╔╝
+╚═╝░░░░░╚═╝░░╚═╝░╚════╝░░╚════╝░░╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═════╝░░╚════╝░
+
+██╗░░░░░██╗██╗░░░██╗██████╗░░█████╗░░██████╗
+██║░░░░░██║██║░░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░░░░██║╚██╗░██╔╝██████╔╝██║░░██║╚█████╗░
+██║░░░░░██║░╚████╔╝░██╔══██╗██║░░██║░╚═══██╗
+███████╗██║░░╚██╔╝░░██║░░██║╚█████╔╝██████╔╝
+╚══════╝╚═╝░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░╚═════╝░''')
+    
+def cadastro_livros():
+    livros = []
+    while True:
+        nome_livro = input('Digite o nome do livro: ').strip()
+        print()
+        livros.append(nome_livro)
+        while True:
+            opcao = input('Quer continuar? [S/N]').strip().upper()
+            print()
+            if opcao in ['N', 'S']:
+                break
+            print('Digite S ou N.')
+        if opcao == 'N':
+            break
+    return livros
+
+def procurar_livro(lista):
+    titulo_livro = input('Digite o livro que quer encontrar: ').strip()
+    for livro in lista:
+        if livro.lower() == titulo_livro.lower():
+            print(f'Livro encontrado: {livro}\n')
+            break
+    else:
+        print('Livro não encontrado.\n')
+
+
+def main():
+    titulo()
+    lista = cadastro_livros()
+    procurar_livro(lista)
+
+if __name__ == '__main__':
+    main()
